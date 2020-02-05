1dec787461bf5ec4eb4eaa0ea981b0ef20a02a07
v_liuyong01 <v_liuyong01@baidu.com>
2020-02-05 20:06:33 +0800

    注释1

---
+++ uitls/src/main/java/com/banban/uitls/UtilsT.java
@@ -0,0 +1,12 @@
+package com.banban.uitls;
+
+/**
+ * create by v_liuyong01
+ * <p>
+ * on      2020/2/5.
+ */
+public class UtilsT {
+    public static String getString(){
+        return "sdddvd";
+    }
+}
