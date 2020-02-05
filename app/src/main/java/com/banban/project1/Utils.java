9dc1aad5b3467f91f920a673af566722ac0ebce7
v_liuyong01 <v_liuyong01@baidu.com>
2020-02-05 17:55:55 +0800

    注释

---
+++ app/src/main/java/com/banban/project1/Utils.java
@@ -0,0 +1,12 @@
+package com.banban.project1;
+
+/**
+ * create by v_liuyong01
+ * <p>
+ * on      2020/2/5.
+ */
+public class Utils {
+    public static String getString(){
+        return "sdddvd";
+    }
+}
